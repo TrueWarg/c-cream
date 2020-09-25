12ac6ccb10ee2b9ed8b452377dc4e478eaa3fbf6
Irrmm <grazashar@mail.ru>
2020-09-25 23:19:38 +0200

    Add simple process

---
+++ src/simple_process.cpp
@@ -0,0 +1,17 @@
+#include <signal.h>
+#include <unistd.h>
+
+#include <fstream>
+
+int main(int argc, char *argv[])
+{
+    std::ofstream file("/home/box/pid");
+    file << getpid();
+    file.close();
+
+    signal(SIGTERM, SIG_IGN);
+    signal(SIGINT, SIG_IGN);
+
+    pause();
+    return 0;
+}
