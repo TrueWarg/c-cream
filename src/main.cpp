dd7f7652151d9803962a92dbfcd80aaee94ede06
Irrmm <grazashar@mail.ru>
2020-09-25 23:21:49 +0200

    Add simple process
    med

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
-
-    char memory[1048576];
-    int kek = 20;
-    void *suka = memory + kek;
-    cout << " ede" << *static_cast<char*>(suka);
-	return 0;
 }
