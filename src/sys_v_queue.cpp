03d3d385c4e8859783013adbf55d9ae24b3ca551
Irrmm <grazashar@mail.ru>
2020-10-02 21:26:15 +0200

    Add SysV queue sample

---
+++ src/sys_v_queue.cpp
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <fcntl.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    const char* key_path = "/tmp/msg.temp";
+    struct { long mtype;char mtext[80]; } message;
+
+    if (creat(key_path, 0666) == -1) {
+        cout << "Error while key file generation" << endl;
+        return -1;
+    }
+
+    key_t key = ftok(key_path, 1);
+
+    int message_id = msgget(key, IPC_CREAT | 0666);
+    if (message_id == -1) {
+        cout << "Error while message receiving" << endl;
+        return -1;
+    }
+
+    int recieved_length = msgrcv(message_id, &message, sizeof(message), 0, 0);
+
+    int output_fd = open("/home/box/message.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    if (output_fd == -1) {
+        cout << "Error while opening output file" << endl;
+        return -1;
+    }
+
+    write(output_fd, message.mtext, recieved_length);
+
+    return 0;
+}
