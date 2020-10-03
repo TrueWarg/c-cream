54e963c4e40ecc4915b24c4b11f04be39734aec4
Irrmm <grazashar@mail.ru>
2020-10-03 13:06:37 +0200

    Add shared memory in SysV example

---
+++ src/sys_v_shared_memory.cpp
@@ -0,0 +1,18 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    const int memory_size = 1024 * 1024;
+    key_t key = ftok("/tmp/mem.temp", 1);
+    int shmid = shmget(key, memory_size, IPC_CREAT | 0666);
+    char *attached = (char *)shmat(shmid, 0, 0);
+    for (int i = 0; i < memory_size; i++)
+    {
+        attached[i] = 42;
+    }
+    // detach from shared memory usinng 'shmdt(attached)' if it's needed
+    return 0;
+}
